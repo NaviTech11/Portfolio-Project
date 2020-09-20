804acd0dc5be9f37df043e9ee976300c285d0e13
Ivan Hernandez <ivan.hdz2@outlook.com>
2020-09-20 18:30:40 -0500

    Add technologies svgs

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 
 
+//Navbar Smart-scroll
+const navBar = document.querySelector(".smart-scroll");
+//Detetc scroll top or down
+window.addEventListener("scroll", () => {
+  navBar.classList.toggle("sticky", window.scrollY > 100)
+})
+
 //Title Effect
 let i = 0;
 let title = "Hi, I'm Ivan";
