4a834c4e98d1fb2aa43a3f2cd5e4ded610bc9763
Ivan Hernandez <ivan.hdz2@outlook.com>
2020-09-25 16:22:05 -0500

    Add ease effect

--- script.js
+++ script.js
@@ -7,22 +7,5 @@ window.addEventListener("scroll", () => {
   navBar.classList.toggle("sticky", window.scrollY > 100)
 })
 
-//Title Effect
-let i = 0;
-let title = "Hi, I'm Ivan";
 
-
-
-function typeWriter(){
-    if (i < title.length) {
-      document.querySelector(".effect").innerHTML += title.charAt(i);
-      i++;
-      setTimeout(typeWriter, 125);
-    }
-}
-
-
-
-
-window.onload = typeWriter;
 
